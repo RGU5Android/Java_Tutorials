8cf629a8a5bb65c23756cd2b5a6664692980f1f0
rgu5android <rgu5android@gmail.com>
2014-05-31 18:53:54 +0530

    Changed :)

--- 01. HelloWorld/src/com/godzplay/helloworld/HelloWorld.java
+++ 01. HelloWorld/src/com/godzplay/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package com.godzplay.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!! :P");
+		System.out.println("Hello World!!! :)");
 		System.out
 				.printf("Playing with GITHUB, Sorry GITHUB for uploading such a foolish project... ");
 	}
