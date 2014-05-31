1a7125819bac8af35e1f9a3a773ea60b5f07d4ef
rgu5android <rgu5android@gmail.com>
2014-05-31 18:23:31 +0530

    adding first Hello World Project

---
+++ 01. HelloWorld/src/com/godzplay/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.godzplay.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!! :P");
+		System.out
+				.printf("Playing with GITHUB, Sorry GITHUB for uploading such a foolish project... ");
+	}
+
+}
